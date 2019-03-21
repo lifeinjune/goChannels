2616dc85c834dfbe679f02cb6c1c76c79069841c
Jun <geffrey90@hotmail.com>
2019-03-22 04:36:33 +0800

    full function without use of channel

--- main.go
+++ main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+)
 
+func main() {
+	address := []string{"http://google.com", "http://facebook.com", "http://stackoverflow.com", "http://golang.org", "http://amazon.com"} //string slcie contain addresses
+	for _, a := range address {                                                                                                           //loop through each element in slice
+		checkAddress(a)
+	}
+}
+
+func checkAddress(a string) {
+	resp, err := http.Get(a) //get respons and error from address given
+	if err != nil {          //if there is error
+		fmt.Println("Error!", err) //print out the error statement
+		return
+	} else { //otherwise
+		fmt.Println(a, "is", resp.Status) //print out address with it's status
+	}
 }
